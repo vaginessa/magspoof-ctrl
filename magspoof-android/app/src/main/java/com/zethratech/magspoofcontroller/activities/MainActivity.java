165a39857a962b25d428c3b21acd3bda891b1591
zethra <jediben97@gmail.com>
2016-04-24 23:57:00 -0400

    Added beggings of android app

---
+++ magspoof-android/app/src/main/java/com/zethratech/magspoofcontroller/activities/MainActivity.java
@@ -0,0 +1,36 @@
+/*
+ * Magspoof Controller - An app to control a Magspoof
+ *     Copyright (C) 2016  Ben Goldberg
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.zethratech.magspoofcontroller.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zethratech.magspoofcontroller.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private String TAG = "MagspoofController - MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ magspoof-android/app/src/main/java/com/zethratech/magspoofcontroller/services/MagspoofServiceImpl.java
@@ -0,0 +1,54 @@
+/*
+ * Magspoof Controller - An app to control a Magspoof
+ *     Copyright (C) 2016  Ben Goldberg
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.zethratech.magspoofcontroller.services;
+
+import android.app.Activity;
+import android.util.Log;
+
+import org.shokai.firmata.ArduinoFirmata;
+
+import java.io.IOException;
+
+public class MagspoofServiceImpl implements MagspoofService{
+
+    private String TAG = "MagspoofController - MagspoofServiceImpl";
+    private ArduinoFirmata arduino = null;
+
+    public void init(Activity activity) {
+        arduino =  new ArduinoFirmata(activity);
+        try{
+            arduino.connect();
+        }
+        catch(IOException | InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void sendID(String id) {
+        Log.v(TAG, "Sending id: " + id);
+        arduino.sysex((byte)0x71, id.getBytes());
+    }
+
+    public void stop() {
+        if(arduino == null)
+            return;
+        arduino.close();
+    }
+
+}
